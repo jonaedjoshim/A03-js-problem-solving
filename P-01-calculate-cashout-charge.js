542f165088d4e05b429993bf074f9bc6223aee7e
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2025-11-22 00:52:40 +0600

    added all js files but dont know how will i solve it

---
+++ P-01-calculate-cashout-charge.js
@@ -0,0 +1,18 @@
+// function cashOut(amount) {
+//     if (isNaN(amount)) {
+//         return "Invalid";
+//     }
+
+//     const number = Number(amount);
+//     if (number <= 0) {
+//         return "Invalid";
+//     }
+
+//     return number;
+// }
+
+
+
+function cashOut (amount){
+    
+}
